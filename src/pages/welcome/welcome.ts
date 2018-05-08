ab24933b89de6a2e355fa8dccaffa37dd165d5f2
Gupta <KGupta2@ad.harman.com>
2018-05-08 16:01:27 +0530

    login-signup starter

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { SignupPage } from '../signup/signup';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html',
+})
+export class WelcomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  login(){
+    this.navCtrl.push(LoginPage);
+  }
+
+  signup(){
+    this.navCtrl.push(SignupPage);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad WelcomePage');
+  }
+
+}
